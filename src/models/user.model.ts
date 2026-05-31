537a381a397116c2527977b8a59ea26429373fa1
Tanmay Singh <tsinghskat@gmail.com>
2026-05-31 14:09:37 +0530

    added pasword hasing feature in model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,8 +1,22 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-export const User = mongoose.model("User", userSchema);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password as string, salt);
+});
+export const User = mongoose.model<IUser>("User", userSchema);
